7f65520b0d6bd737478336d989592db7ba771ee7
Jacob Schatz <jschatz@fastmail.com>
2026-02-05 21:01:22 +0200

    Add CI navigation link to repo page

--- kailab-control/frontend/src/routes/orgs/[slug]/[repo]/+page.svelte
+++ kailab-control/frontend/src/routes/orgs/[slug]/[repo]/+page.svelte
@@ -144,6 +144,12 @@
 				</svg>
 				History
 			</a>
+			<a href="/orgs/{slug}/{repo}/workflows" class="btn btn-secondary">
+				<svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+				</svg>
+				CI
+			</a>
 		</div>
 	</div>
 
