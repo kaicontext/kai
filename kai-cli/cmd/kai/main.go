7d2dc32e08cd7933f00045fccdcd7bc920956fe0
Jacob Schatz <jschatz@fastmail.com>
2026-03-26 13:01:28 +0200

    Bump version to 0.9.22

--- kai-cli/cmd/kai/main.go
+++ kai-cli/cmd/kai/main.go
@@ -68,7 +68,7 @@ const (
 )
 
 // Version is the current kai CLI version
-var Version = "0.9.21"
+var Version = "0.9.22"
 
 // verbose enables debug output when --verbose/-v flag or KAI_VERBOSE env var is set
 var verbose bool
