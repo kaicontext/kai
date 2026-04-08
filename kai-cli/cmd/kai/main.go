16087185e3d7f723a68b7786c1e6d47e8e0daf74
Jacob Schatz <jschatz@fastmail.com>
2026-04-08 19:07:04 +0300

    Bump version to 0.9.71

--- kai-cli/cmd/kai/main.go
+++ kai-cli/cmd/kai/main.go
@@ -69,7 +69,7 @@ const (
 )
 
 // Version is the current kai CLI version
-var Version = "0.9.70"
+var Version = "0.9.71"
 
 // verbose enables debug output when --verbose/-v flag or KAI_VERBOSE env var is set
 var verbose bool
