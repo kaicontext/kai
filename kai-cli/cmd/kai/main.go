3ea7809e6503f826289d81bc8c97fbc3b07681ec
Jacob Schatz <jschatz@fastmail.com>
2026-04-13 16:05:12 +0300

    v0.9.99: Fix MCP server crash on every file edit

--- kai-cli/cmd/kai/main.go
+++ kai-cli/cmd/kai/main.go
@@ -70,7 +70,7 @@ const (
 )
 
 // Version is the current kai CLI version
-var Version = "0.9.98"
+var Version = "0.9.99"
 
 // verbose enables debug output when --verbose/-v flag or KAI_VERBOSE env var is set
 var verbose bool
