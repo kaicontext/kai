0cf24063fb20aaa1a3c1f497c182a515ba5c1c8e
Jacob Schatz <jschatz@fastmail.com>
2026-04-20 15:33:56 +0300

    kai 0.12.2: colorized kai diff output

--- kai-cli/cmd/kai/main.go
+++ kai-cli/cmd/kai/main.go
@@ -70,7 +70,7 @@ const (
 )
 
 // Version is the current kai CLI version
-var Version = "0.12.1"
+var Version = "0.12.2"
 
 // verbose enables debug output when --verbose/-v flag or KAI_VERBOSE env var is set
 var verbose bool
